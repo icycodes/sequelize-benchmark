1cd5a28d81f93e8b8bf7f16206a720b358c5bf70
Zhiming Ma <codes.icy@gmail.com>
2026-05-29 00:38:59 +0800

    data: Phase 2 tasks.

--- tasks/bulk_create_individual_hooks/environment/test_initial_state.py
+++ tasks/bulk_create_individual_hooks/environment/test_initial_state.py
@@ -1,13 +1,16 @@
 import os
 import shutil
+import pytest
 
-PROJECT_DIR = "/home/user/project"
+PROJECT_DIR = "/home/user/myproject"
 
-def test_node_available():
+def test_node_and_npm_available():
     assert shutil.which("node") is not None, "node binary not found in PATH."
-
-def test_npm_available():
     assert shutil.which("npm") is not None, "npm binary not found in PATH."
 
 def test_project_dir_exists():
     assert os.path.isdir(PROJECT_DIR), f"Project directory {PROJECT_DIR} does not exist."
+
+def test_package_json_exists():
+    package_json_path = os.path.join(PROJECT_DIR, "package.json")
+    assert os.path.isfile(package_json_path), f"File {package_json_path} does not exist."
--- tasks/nested_includes_filtered/environment/test_initial_state.py
+++ tasks/nested_includes_filtered/environment/test_initial_state.py
@@ -1,8 +1,7 @@
 import os
 import shutil
-import pytest
 
-PROJECT_DIR = "/home/user/project"
+PROJECT_DIR = "/home/user/myproject"
 
 def test_node_available():
     assert shutil.which("node") is not None, "node binary not found in PATH."
@@ -10,8 +9,5 @@ def test_node_available():
 def test_npm_available():
     assert shutil.which("npm") is not None, "npm binary not found in PATH."
 
-def test_sqlite3_available():
-    assert shutil.which("sqlite3") is not None, "sqlite3 binary not found in PATH."
-
-def test_project_dir_exists():
+def test_project_directory_exists():
     assert os.path.isdir(PROJECT_DIR), f"Project directory {PROJECT_DIR} does not exist."
