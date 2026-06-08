593fd69d02d60b9455638dd64f18af727b5ccff7
Zhiming Ma <codes.icy@gmail.com>
2026-06-08 10:11:19 +0800

    data: filter tasks.

--- jobs/2026-05-29__05-42-30/dynamic_connection_hooks__wNAPa59/artifacts/user/myproject/test.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize({ dialect: 'sqlite', storage: ':memory:' });
-
-sequelize.beforeConnect(() => {
-  console.log('beforeConnect triggered');
-});
-
-sequelize.authenticate().then(() => console.log('authenticated'));
